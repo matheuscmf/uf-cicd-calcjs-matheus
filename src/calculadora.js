91a944813b702f5292359c872782d1f7b67778d3
Matheus Marques <matheuscmf1995@gmail.com>
2024-09-14 15:44:06 +0000

    commit 5

--- src/calculadora.js
+++ src/calculadora.js
@@ -7,8 +7,17 @@ return a + b;
 function subtract(a, b) {
 return a - b;
 }
+// Função de divisão
+function divide(a, b) {
+if (b === 0) {
+throw new Error("Divisão por zero não é permitida.");
+}
+return a / b;
+}
 // Exportar as funções
 module.exports = {
 add,
-subtract
+subtract,
+divide,
 };
+
